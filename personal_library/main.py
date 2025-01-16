837f186297c82ed51c8cb26a1ec13add7c1251e7
Parryhip <samuel.andelin@ucas-edu.net>
2025-01-16 10:52:07 -0700

    stuff

--- personal_library/main.py
+++ personal_library/main.py
@@ -1,50 +1,70 @@
 #Samuel Andelin, Personal Library Program
 
-#list containing all books
-books = []
+#list containing all books corrresponding to their authors
+library = set({})
 
-#list containing all authors
-authors = {}
+#list containing corresponding values of books to authors
+nums = []
 
 #add book function
 def add_book():
     book_name = input("What is the name of the book you want to add?\n-->")
     author = input("What is the name of the author of the book?\n-->")
-    books.append(book_name.lower())
-    author.add(author)
+    library.add((book_name, author))
 
 #remove book function
 def remove_book():
     while True:
         print("These are the names of all of the books that you have: ")
         num = 1
-        for book in books:
-            print(f"{num}. {book}")
-            num += 1
+        for book in library:
+            for item in book:
+                print(f"{num}. {book}")
+                num += 1
         book_name = input("What is the name of the book you want to remove? (Or type 1 for exit)\n-->")
-        if book_name in books:
-            books.remove(book_name.lower())
-            print(f"Removed {book_name}.")
-            break
-        elif book_name == "1":
-            break
-        else:
-            print("That is not an item in your library!")
+        for book in library:
+            if book_name in book:
+                library.remove(book)
+                print(f"Removed {book_name}.")
+                break
+            elif book_name == "1":
+                break
+            else:
+                print("That is not an item in your library!")
 
 #search library function
 def search():
     while True:
-        type_of_search = input("Type 1 to search by book name, type 2 to search by author.\n-->")
+        type_of_search = input("Type 1 to search by book name, type 2 to search by author, type 3 to exit.\n-->")
         if type_of_search == "1":
             book_search = input("What is the name of the book you want to search for?\n-->")
             num = 0
             searched_books = []
-            for book in books:
-                if book_search.lower() in book:
-                    num += 1
-                    searched_books.append(book)
+            for book in library:
+                for item in book:
+                    if book_search.lower() in item:
+                        num += 1
+                        searched_books.append(book)
             print(f"{num} results for {book_search}:")
-                            
+            for i in library:
+                string_stuff = ", ".join(library(i))
+            print("\n".join(string_stuff))
+            break
+        elif type_of_search == "2":
+            author_search = input("What is the name of the author?\n--> ")
+            num2 = 0
+            searched_authors = []
+            for book in library:
+                for author in book:
+                    if author_search in author:
+                        num2 += 1
+                        searched_authors.append(author)
+            print(f"{num2} results for {author_search}:")
+            print("\n".join(searched_authors))
+            which_author = input("Type the number that is by the author whose books you want to look at.\n-->")
+
+        elif type_of_search == "3":
+            break
 #User Interface function
 def main():
     print("Welcome to the Online Personal Libary for Books!")
@@ -57,3 +77,13 @@ def main():
             add_book()
         elif choice == "2":
             remove_book()
+        elif choice == "3":
+            search()
+        elif choice == "4":
+            print("Ok, bye!")
+            break
+        else:
+            print("Not a valid input!")
+
+
+main()
