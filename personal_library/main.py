7f6d0a51fbb8a4232e452539e6eb062ae469d120
Samuel Andelin <samuel.andelin@ucas-edu.net>
2025-02-03 19:02:43 -0700

    initial

--- personal_library/main.py
+++ personal_library/main.py
@@ -86,7 +86,6 @@ def search():
                 num4 += 1
         elif type_of_search == "3":
             break
-
 #User Interface function
 def main():
     print("Welcome to the Online Personal Libary for Books!")
